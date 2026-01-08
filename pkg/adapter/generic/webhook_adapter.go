a89f20feb566e466bfcb3620990707765dc60ee4
Kube-ZEN: Zero-Effort KuberNetes <zen@kube-zen.io>
2026-01-08 04:12:08 -0500

    fix: completely remove secrets package dependency
    - Removed all secretCache references
    - Removed unused metav1 import
    - Fixes compilation errors

--- pkg/adapter/generic/webhook_adapter.go
+++ pkg/adapter/generic/webhook_adapter.go
@@ -33,7 +33,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
